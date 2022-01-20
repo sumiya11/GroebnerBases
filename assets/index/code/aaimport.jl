2ddc47e72e97996d631f5d68c08c896eb0f81972
sumiya11 <60229118+sumiya11@users.noreply.github.com>
2022-01-20 15:18:14 +0000

    Deploying to gh-pages from  @ 24bfed6f2b3800036a040823025052b172c47461 🚀

---
+++ assets/index/code/aaimport.jl
@@ -0,0 +1,7 @@
+# This file was generated, do not modify it. # hide
+using DynamicPolynomials
+@polyvar x1 x2
+system = [10*x1*x2^2 - 11*x1 + 10,
+        10*x1^2*x2 - 11*x2 + 10]
+
+groebner(system)
