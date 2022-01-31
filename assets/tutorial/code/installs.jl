1ac85513ca1319a6adc5ba55cda45ecbecf4f56b
sumiya11 <60229118+sumiya11@users.noreply.github.com>
2022-01-31 22:14:45 +0000

    Deploying to gh-pages from  @ 4ed328865fa407149ed39eb828650d4f87cb1613 🚀

--- assets/tutorial/code/installs.jl
+++ assets/tutorial/code/installs.jl
@@ -1,5 +1,6 @@
 # This file was generated, do not modify it. # hide
 import Pkg # hide
+import Pkg; Pkg.add("BenchmarkTools") # hide
 Pkg.add("AbstractAlgebra") # hide
 Pkg.add(url="https://github.com/sumiya11/Groebner.jl") # hide
 Pkg.add("DynamicPolynomials") # hide
