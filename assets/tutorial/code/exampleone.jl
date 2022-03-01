3c5530afdefec43a2c702e718765ccb789b95b63
sumiya11 <60229118+sumiya11@users.noreply.github.com>
2022-03-01 12:26:30 +0000

    Deploying to gh-pages from  @ 170d4cc389a20d2aa299424219c31510fef2c080 🚀

--- assets/tutorial/code/exampleone.jl
+++ assets/tutorial/code/exampleone.jl
@@ -1,4 +1,3 @@
 # This file was generated, do not modify it. # hide
 using Groebner
 G = groebner(F)
-println("Basis is $G \nLeading Ideal is $(map(leading_term, G))")
--- assets/tutorial/code/output/exampleone.res
+++ assets/tutorial/code/output/exampleone.res
@@ -1,1 +1,4 @@
-nothing
+3-element Vector{AbstractAlgebra.Generic.MPoly{Rational{BigInt}}}:
+ y^3 - y^2
+ x*y^2 + y^2
+ x^2 + x*y
