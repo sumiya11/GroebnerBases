7d2afdf594528731103178e965c287454b5cfab9
sumiya11 <60229118+sumiya11@users.noreply.github.com>
2022-05-02 23:26:34 +0000

    Deploying to gh-pages from  @ 3ef13895068cccd67eb2e8c0901a03d81760f20a 🚀

--- assets/tutorial/code/graph1.jl
+++ assets/tutorial/code/graph1.jl
@@ -1,4 +1,7 @@
 # This file was generated, do not modify it. # hide
 # hideall
-gplot(g)
-draw(SVG(joinpath(@OUTPUT, "graph.svg"), 16cm, 16cm), gplot(g));
+xs = [0.5, 1.0, 0.5, 0.0]
+ys = [-sqrt(3) / 6, 0.0, -1.0, 0.0]
+nodelabeldist = 1.5
+nodelabel = collect(1:4)
+draw(SVG(joinpath(@OUTPUT, "graph.svg"), 16cm, 16cm), gplot(g, xs, ys, nodelabel=nodelabel, nodelabeldist=nodelabeldist));
--- assets/tutorial/code/graph2.jl
+++
@@ -1,2 +0,0 @@
-# This file was generated, do not modify it. # hide
-draw(SVG(joinpath(@OUTPUT, "graph.svg"), 16cm, 16cm), gplot(g));
