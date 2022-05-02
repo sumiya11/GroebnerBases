0e2f7d8cd52743d51701c04b397c007ea36a2642
sumiya11 <60229118+sumiya11@users.noreply.github.com>
2022-05-02 23:35:05 +0000

    Deploying to gh-pages from  @ 48fec2523962f0a7910bc32ad002a8385dbb7bfd 🚀

--- assets/tutorial/code/graph1.jl
+++ assets/tutorial/code/graph1.jl
@@ -4,4 +4,4 @@ xs = [0.5, 1.0, 0.5, 0.0]
 ys = [-sqrt(3) / 6, 0.0, -1.0, 0.0]
 nodelabeldist = 1.5
 nodelabel = collect(1:4)
-draw(SVG(joinpath(@OUTPUT, "graph.svg"), 16cm, 16cm), gplot(g, xs, ys, nodelabel=nodelabel, nodelabeldist=nodelabeldist));
+draw(SVG(joinpath(@OUTPUT, "graph2.svg"), 16cm, 16cm), gplot(g, xs, ys, nodelabel=nodelabel, nodelabeldist=nodelabeldist));
