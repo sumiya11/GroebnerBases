1b9cbf6a56a486a8be739800849898d7db5a28bf
sumiya11 <60229118+sumiya11@users.noreply.github.com>
2023-09-17 07:33:20 +0000

    Deploying to gh-pages from  @ 282d4222b33075b20ad621d9144fd335ea3523f3 🚀

--- assets/tutorial/code/installs.jl
+++ assets/tutorial/code/installs.jl
@@ -4,7 +4,7 @@ import Pkg; Pkg.add("BenchmarkTools") # hide
 Pkg.add("Graphs") # hide
 Pkg.add("GraphPlot") # hide
 Pkg.add("AbstractAlgebra") # hide
-Pkg.add("Groebner") # hide
+# Pkg.add("Groebner") # hide
 Pkg.add("DynamicPolynomials") # hide
 Pkg.add("AbstractAlgebra") # hide
 Pkg.add("Compose") # hide
