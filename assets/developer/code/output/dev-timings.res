fe440e9d5a738d98576590d4a954f3a0f561d204
sumiya11 <60229118+sumiya11@users.noreply.github.com>
2023-10-27 02:17:22 +0000

    Deploying to gh-pages from  @ e680794e808943b6172935ab0ef46cfd3113474e 🚀

--- assets/developer/code/output/dev-timings.res
+++ assets/developer/code/output/dev-timings.res
@@ -1,1 +1,4 @@
-nothing
+3-element Vector{AbstractAlgebra.Generic.MPoly{AbstractAlgebra.GFElem{Int64}}}:
+ y^2 + 2147483646*z^2
+ x*z + y
+ x*y + z
---
+++ assets/index/code/install-nightly-groebner.jl
@@ -0,0 +1,3 @@
+# This file was generated, do not modify it. # hide
+using Pkg; # hide
+Pkg.add(url="https://github.com/sumiya11/Groebner.jl") # hide
