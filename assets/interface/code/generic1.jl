097f04fcb5de3d4cbda943b133a7a709cf71badd
sumiya11 <60229118+sumiya11@users.noreply.github.com>
2025-01-04 00:15:43 +0000

    Deploying to gh-pages from  @ a5beb7e939eed5753e8bb671a47473bb64a456b6 🚀

---
+++ assets/interface/code/generic1.jl
@@ -0,0 +1,10 @@
+# This file was generated, do not modify it. # hide
+using Groebner, Nemo
+
+R, t = GF(101, 7)["t"]
+ff = fraction_field(R)
+_, (x, y) = ff["x","y"]
+
+sys = [(t//t+1)*x*y - t^3, y^2 + t]
+
+gb = groebner(sys)
